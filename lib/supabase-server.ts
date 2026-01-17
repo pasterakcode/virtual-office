1cc28499c12dd197bee7aca2b7e3dc87615ccdbb
pasterakcode <78322363+pasterakcode@users.noreply.github.com>
2026-01-17 23:09:01 +0100

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SERVICE KEY", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 export const supabaseServer = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
